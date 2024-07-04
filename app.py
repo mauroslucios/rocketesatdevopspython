8000b8307e153b9a579fb37747307ad3399e6373
mauroslucios <mauroslucios@gmail.com>
2024-07-03 22:12:40 -0300

    rotas update e get_all

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # Importando a classe FLASK
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 # Criando instância da classe Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
 # Modelagem
 
@@ -35,6 +37,52 @@ def delete_product(product_id):
         db.session.commit()
         return jsonify({"message":"Product deleted successfully."})
     return jsonify({"message":"Product not found."}), 404
+
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message":"Product not found"}), 404
+
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message":"Product not found."}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+    
+    if 'description' in data:
+        product.description = data['description']
+
+    db.session.commit()
+    return jsonify({"message":"Product updated successfully."})
+    
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        produtc_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+        }
+        product_list.append(produtc_data)
+    
+    return jsonify(product_list)
 
 @app.route('/')
 def hello_world():
